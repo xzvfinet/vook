f53459b747abc075a20f6b13be88c937b4eb5400
xzvfinet <xzvfinet@gmail.com>
2016-12-02 18:09:22 +0900

    multi page app

---
+++ pages/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Page(models.Model):
+    next_page = models.ForeignKey('Page', blank=True, null=True)
+
+    index = models.IntegerField()
+    image_file = models.ImageField(upload_to='static_files/uploaded/original/%Y/%m/%d')
+
+    def delete(self, *args, **kwargs):
+        self.image_file.delete()
+
+
---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Page
+
+
+def page_view(request, page_number):
+    page = get_object_or_404(Page, index=page_number)
+
+    return render(request, 'page.html',
+                  {
+                      'page_index': page.index,
+                      'img_url': page.image_file.url,
+                      'next_page': page.next_page
+                  })
